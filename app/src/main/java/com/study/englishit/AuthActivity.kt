eb237104ae680555775e02247cc9f98e5a55356b
Dario DAngelo <dariodangelo21@gmail.com>
2023-01-19 16:40:42 -0300

    Email in Used FINALLYYYY

--- app/src/main/java/com/study/englishit/AuthActivity.kt
+++ app/src/main/java/com/study/englishit/AuthActivity.kt
@@ -3,9 +3,13 @@ package com.study.englishit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.ktx.Firebase
 import com.study.englishit.databinding.AuthActivityBinding
 import com.study.englishit.util.Constants.USER_EMAIL_FB
@@ -22,7 +26,6 @@ class AuthActivity : AppCompatActivity() {
         //variables
 
 
-
         //Sign Up
         binding.btnSignUp.setOnClickListener {
             val etEmail = binding.etEmail.text.toString()
@@ -31,16 +34,27 @@ class AuthActivity : AppCompatActivity() {
                 FirebaseAuth.getInstance()
                     .createUserWithEmailAndPassword(etEmail, etPassword)
                     .addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            navigateToHome(
-                                email = it.result.user?.email ?: ""
-                            )
-                        } else {
-                            showFailedRegister()
+                        try {
+                            val msjError =
+                                "com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account"
+                            //
+                            if (it.isSuccessful) {
+                                navigateToHome(it.result.user?.email ?: "")
+                            } else if (it.exception?.toString()
+                                    ?.contains(msjError) == true
+                            ) {
+                                showUsed()
+                            } else {
+                                showFailedRegister()
+                            }
+                        } catch (e: FirebaseAuthException) {
+                            Toast.makeText(this, "Something woes wrong", Toast.LENGTH_LONG).show()
                         }
                     }
             }
         }
+
+
         //Log In
         binding.btnLogin.setOnClickListener {
             val etEmail = binding.etEmail.text.toString()
@@ -69,15 +83,25 @@ class AuthActivity : AppCompatActivity() {
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+
     private fun showFailedRegister() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Check your forms")
         builder.setMessage("The password has to be 6 character minimum, and check that the email was correctly written")
-        builder.setPositiveButton("Accept", null)
+        builder.setPositiveButton("Understand", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
 
+
+    private fun showUsed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Email Already in Use")
+        builder.setMessage("The email you attempt to use is already taken , please choose another.")
+        builder.setPositiveButton("Ok, i'll change it", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 
 
     private fun navigateToHome(email: String) {
