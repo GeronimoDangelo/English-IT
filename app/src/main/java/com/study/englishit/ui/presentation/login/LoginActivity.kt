1dae03e28deb72ae34fa072f387a5d4ca8e54fc5
Dario DAngelo <dariodangelo21@gmail.com>
2023-01-20 14:17:16 -0300

    all working and connected

---
+++ app/src/main/java/com/study/englishit/ui/presentation/login/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.study.englishit.ui.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.study.englishit.R
+import com.study.englishit.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_EnglishIT)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/study/englishit/ui/presentation/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.study.englishit.ui.presentation.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.study.englishit.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
